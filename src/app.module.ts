2a9a30fc4e02ea8de4c06b93127414570faf90f1
Arick Bulakali <arickbulakali@gmail.com>
2024-10-10 13:38:26 +0200

    refactor: add middleware config, enable cors, add app environnement config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
+import { MorganMiddleware } from '@nest-middlewares/morgan';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+
+    MorganMiddleware.configure('dev')
+    consumer.apply( HelmetMiddleware,MorganMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const corsOptions = {
+    origin:['http://localhost:4200'],
+  }
+  app.enableCors(corsOptions)
+  const PORT = configService.get('APP_PORT') || 3500;
+  await app.listen(PORT);
 }
 bootstrap();
