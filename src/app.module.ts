32b008aba26c962f9b088de6a4d384a472f5e145
Arick Bulakali <arickbulakali@gmail.com>
2024-10-10 14:20:50 +0200

    refactor: add DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { HelmetMiddleware } from '@nest-middlewares/helmet';
 import { MorganMiddleware } from '@nest-middlewares/morgan';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,6 +12,16 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:process.env.DB_HOST,
+      database:process.env.DB_NAME,
+      port:parseInt(process.env.DB_PORT),
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      entities:[], // Array of entities to load
+      synchronize:process.env.NODE_ENV==='development' // For development only, not for production, permet que toute modification sur les models(Au niveau des entites TypeORM) soit directement répercutée sur la base de données.
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
