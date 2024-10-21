82dcd1ffc654f6f4aa6307b564664d95906f620d
Ndekocode <arickbulakali@gmail.com>
2024-10-21 08:55:13 +0200

    refactor: add repository pattern and get cv

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { CvService } from './cv.service';
 
 @Controller('cv')
-export class CvController {}
+export class CvController {
+  constructor(private readonly cvService: CvService) {}
+  @Get()
+  getCvs() {
+    return this.cvService.getCvs();
+  }
+}
--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,18 @@
+import { Repository } from 'typeorm';
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CvEntity } from './entities/cv.entity';
 
 @Injectable()
-export class CvService {}
+export class CvService {
+  constructor(
+    @InjectRepository(CvEntity) private readonly cvRepository: Repository<CvEntity>,
+  ) {}
+
+  // Je veux avoir une promesse d'avoir des CV-Entity
+  async getCvs():Promise<CvEntity[]>{
+    return await this.cvRepository.find()
+  }
+}
